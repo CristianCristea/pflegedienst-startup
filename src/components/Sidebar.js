e2e6b11f6f02c26afede5c496aedc4d9781c11ed
Cristian Cristea <cristiancristea.1987@gmail.com>
2017-10-08 12:21:41 +0200

    Add link

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -16,6 +16,16 @@ const Sidebar = () => {
               <FontAwesome name="book" tag="i" />
             </a>
           </li>
+          <li>
+            <a href="/" className="nav-link">
+              <FontAwesome
+                name="ban"
+                tag="i"
+                tag="i"
+                style={{ color: 'red' }}
+              />
+            </a>
+          </li>
         </ul>
       </nav>
     </aside>
